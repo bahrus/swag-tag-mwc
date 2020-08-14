7fc1336a0a0f050b8fb2f5ccdcd372387c0395e5
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-14 03:38:14 -0400

    ...

--- swag-tag-catalog-mwc.js
+++ swag-tag-catalog-mwc.js
@@ -4,14 +4,72 @@ import { createTemplate } from 'trans-render/createTemplate.js';
 import('xtal-fetch/xtal-fetch-req.js');
 import { templStampSym } from 'trans-render/plugins/templStamp.js';
 import('./swag-tag-mwc');
+import('@material/mwc-top-app-bar/mwc-top-app-bar.js');
+import('@material/mwc-icon-button/mwc-icon-button.js');
 const mainTemplate = createTemplate(/* html */ `
+    <style>
+        :host {
+            height: 100vh;
+        }
+
+        .drawer-content {
+            padding: 0px 16px 0 16px;
+        }
+
+        .main-content {
+            min-height: 300px;
+            padding: 48px 18px 0 18px;
+        }
+
+        [inert] {
+            pointer-events: none;
+            cursor: default;
+        }
+
+        [inert],
+        [inert] * {
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        :host {
+  font-family: Roboto, sans-serif;
+  margin: 0;
+  transition: opacity ease-in 0.2s;
+}
+
+main {
+  padding: 16px;
+}
+
+.demo-group, .demo-group-spaced {
+  display: flex;
+  align-items: center;
+}
+
+.wrap {
+  flex-wrap: wrap;
+}
+
+.demo-group-spaced {
+  justify-content: space-around;
+}
+
+.demo-group > *, .demo-group-spaced > * {
+  margin: 0 8px;
+}
+
+    </style>
+    <slot name=linkList part=linksSlot style=display:none></slot>
     <mwc-drawer hasheader type="modal" open>
         <span slot="title">Drawer Title</span>
         <span slot="subtitle">subtitle</span>
         <div class="drawer-content">
             <p>Drawer content</p>
-            <mwc-icon-button icon="gesture"></mwc-icon-button>
-            <mwc-icon-button icon="gavel" id="gavel"></mwc-icon-button>
+            <div part=mwcButtonContainer></div>
+            <!-- <mwc-icon-button icon="device_hub" title="" data-href=></mwc-icon-button>
+            <mwc-icon-button icon="gavel" id="gavel"></mwc-icon-button> -->
         </div>
         <div slot="appContent">
             <mwc-top-app-bar>
@@ -24,20 +82,41 @@ const mainTemplate = createTemplate(/* html */ `
                 <xtal-fetch-req fetch -href as=text part=fetch insert-results></xtal-fetch-req>
             </div>
         </div>
+        
     </mwc-drawer>
+    
 `);
-const uiRefs = { fetch: Symbol('fetch') };
-const initTransform = {
+const uiRefs = { fetch: Symbol('fetch'), linksSlot: Symbol('linksSlot'), mwcButtonContainer: Symbol('mwcButtonContainer') };
+const initTransform = ({ onLinksSlotChange }) => ({
     ':host': [templStampSym, uiRefs],
+    [uiRefs.linksSlot]: [{}, { slotchange: onLinksSlotChange }]
+});
+const linkLinks = ({ linkAssignedNodes, self }) => {
+    const links = [];
+    Array.from(linkAssignedNodes).forEach(node => {
+        if (node.querySelectorAll) {
+            Array.from(node.querySelectorAll('a')).forEach(a => {
+                links.push(a);
+            });
+        }
+    });
+    self.links = links;
 };
-const bindFetch = ({ href }) => ({
-    [uiRefs.fetch]: [{ href: href }],
+const drawerButton = createTemplate(/* html */ `
+<mwc-icon-button icon="device_hub" title=""></mwc-icon-button>
+`);
+const bindLinks = ({ links }) => ({
+    [uiRefs.mwcButtonContainer]: [links, drawerButton, , {
+            'mwc-icon-button': ({ target, item }) => {
+                target.dataset.href = item.href;
+            }
+        }],
 });
 const updateTransforms = [
-    bindFetch,
+    bindLinks,
 ];
 const propActions = [
-    bindFetch,
+    linkLinks,
 ];
 export class SwagTagCatalogMWC extends XtalElement {
     constructor() {
@@ -49,9 +128,14 @@ export class SwagTagCatalogMWC extends XtalElement {
         this.updateTransforms = updateTransforms;
         this.propActions = propActions;
     }
+    onLinksSlotChange() {
+        //setTimeout(() =>{
+        this.linkAssignedNodes = this[uiRefs.linksSlot].assignedNodes();
+        //}, 1000);
+    }
 }
 SwagTagCatalogMWC.is = 'swag-tag-catalog-mwc';
-SwagTagCatalogMWC.attributeProps = ({ href }) => ({
-    str: [href],
+SwagTagCatalogMWC.attributeProps = ({ linkAssignedNodes, links }) => ({
+    obj: [linkAssignedNodes, links],
 });
 define(SwagTagCatalogMWC);
--- swag-tag-catalog-mwc.ts
+++ swag-tag-catalog-mwc.ts
@@ -1,18 +1,78 @@
 import("@material/mwc-drawer/mwc-drawer.js");
-import { XtalElement, define, AttributeProps } from 'xtal-element/XtalElement.js';
+import { XtalElement, define, AttributeProps, TransformGetter, SelectiveUpdate, RenderContext } from 'xtal-element/XtalElement.js';
 import { createTemplate } from 'trans-render/createTemplate.js';
 import('xtal-fetch/xtal-fetch-req.js');
 import { templStampSym } from 'trans-render/plugins/templStamp.js';
 import { TransformValueOptions } from 'trans-render/types.d.js';
+import { SwagTag } from '../swag-tag/swag-tag';
 import('./swag-tag-mwc');
+import('@material/mwc-top-app-bar/mwc-top-app-bar.js');
+import('@material/mwc-icon-button/mwc-icon-button.js');
+
 const mainTemplate = createTemplate(/* html */`
+    <style>
+        :host {
+            height: 100vh;
+        }
+
+        .drawer-content {
+            padding: 0px 16px 0 16px;
+        }
+
+        .main-content {
+            min-height: 300px;
+            padding: 48px 18px 0 18px;
+        }
+
+        [inert] {
+            pointer-events: none;
+            cursor: default;
+        }
+
+        [inert],
+        [inert] * {
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        :host {
+  font-family: Roboto, sans-serif;
+  margin: 0;
+  transition: opacity ease-in 0.2s;
+}
+
+main {
+  padding: 16px;
+}
+
+.demo-group, .demo-group-spaced {
+  display: flex;
+  align-items: center;
+}
+
+.wrap {
+  flex-wrap: wrap;
+}
+
+.demo-group-spaced {
+  justify-content: space-around;
+}
+
+.demo-group > *, .demo-group-spaced > * {
+  margin: 0 8px;
+}
+
+    </style>
+    <slot name=linkList part=linksSlot style=display:none></slot>
     <mwc-drawer hasheader type="modal" open>
         <span slot="title">Drawer Title</span>
         <span slot="subtitle">subtitle</span>
         <div class="drawer-content">
             <p>Drawer content</p>
-            <mwc-icon-button icon="gesture"></mwc-icon-button>
-            <mwc-icon-button icon="gavel" id="gavel"></mwc-icon-button>
+            <div part=mwcButtonContainer></div>
+            <!-- <mwc-icon-button icon="device_hub" title="" data-href=></mwc-icon-button>
+            <mwc-icon-button icon="gavel" id="gavel"></mwc-icon-button> -->
         </div>
         <div slot="appContent">
             <mwc-top-app-bar>
@@ -25,35 +85,65 @@ const mainTemplate = createTemplate(/* html */`
                 <xtal-fetch-req fetch -href as=text part=fetch insert-results></xtal-fetch-req>
             </div>
         </div>
+        
     </mwc-drawer>
+    
 `);
-const uiRefs = {fetch: Symbol('fetch')};
-const initTransform = {
+const uiRefs = { fetch: Symbol('fetch'), linksSlot: Symbol('linksSlot'), mwcButtonContainer: Symbol('mwcButtonContainer') };
+const initTransform = ({ onLinksSlotChange }: SwagTagCatalogMWC) => ({
     ':host': [templStampSym, uiRefs],
-} as TransformValueOptions;
+    [uiRefs.linksSlot]: [{}, { slotchange: onLinksSlotChange }]
+} as TransformValueOptions);
 
-const bindFetch = ({href}: SwagTagCatalogMWC) =>({
-    [uiRefs.fetch]: [{href: href}],
+const linkLinks = ({ linkAssignedNodes, self }: SwagTagCatalogMWC) => {
+    const links: HTMLAnchorElement[] = [];
+    Array.from(linkAssignedNodes!).forEach(node => {
+        if((<any>node).querySelectorAll) {
+            Array.from((<Element>node).querySelectorAll('a')).forEach(a =>{
+                links.push(a);
+            })
+        }
+    });
+    self.links = links;
+}
+const drawerButton = createTemplate(/* html */`
+<mwc-icon-button icon="device_hub" title=""></mwc-icon-button>
+`);
+const bindLinks = ({links}: SwagTagCatalogMWC) =>({
+    [uiRefs.mwcButtonContainer]: [links, drawerButton,,{
+        'mwc-icon-button': ({target, item}: RenderContext<SwagTagCatalogMWC, HTMLAnchorElement>) => {
+            target!.dataset.href = item!.href;
+        } 
+    }],
 });
 const updateTransforms = [
-    bindFetch,
-]
+    bindLinks,
+] as SelectiveUpdate<any>[];
 const propActions = [
-    bindFetch,
+    linkLinks,
 ];
+
+
 
 export class SwagTagCatalogMWC extends XtalElement {
     static is = 'swag-tag-catalog-mwc';
-    static attributeProps = ({href}: SwagTagCatalogMWC) => ({
-        str:[href],
+    static attributeProps = ({ linkAssignedNodes, links }: SwagTagCatalogMWC) => ({
+        obj: [linkAssignedNodes, links],
     } as AttributeProps);
     readyToInit = true;
     readyToRender = true;
     mainTemplate = mainTemplate;
     initTransform = initTransform;
     updateTransforms = updateTransforms;
-    href: string | undefined;
+    links: HTMLAnchorElement[] | undefined;
+    linkAssignedNodes: Node[] | undefined;
     propActions = propActions;
+    onLinksSlotChange() {
+        //setTimeout(() =>{
+            this.linkAssignedNodes = (<any>this)[uiRefs.linksSlot].assignedNodes();
+        //}, 1000);
+        
+    }
 }
 
 define(SwagTagCatalogMWC);
